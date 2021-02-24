f8a8ac8f10536d0c089abcb801d84a740578cf38
Max <48175311+rast1ch@users.noreply.github.com>
2021-02-25 00:06:40 +0200

    Updated comments

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
-import telebot
-from gtts import gTTS
+import telebot # підключення бібліотеки для роботи з Телеграм ботом
+from gtts import gTTS # підключення бібліотеки для перетворення тексту в Аудіо
 
 
-TOCKEN  = '*'
+TOCKEN  = 'АPI-код, отриманий від BotFather'
 
-language = 'uk'
-bot = telebot.TeleBot(TOCKEN)
+language = 'uk' # Стандартна мова обробки 
+bot = telebot.TeleBot(TOCKEN) # Створення екземпляру бота 
 
 @bot.message_handler(content_types = ['text'])
 def get_text_messages(message):
-    global language
-    if message.text == "/start":
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        key_ru = telebot.types.InlineKeyboardButton(text = "Українська", callback_data = 'uk')
-        keyboard.add(key_ru)
-        key_en = telebot.types.InlineKeyboardButton(text = "English", callback_data = 'en')
-        keyboard.add(key_en)
+    global language # Створення доступу до загального налаштування мови
+    if message.text == "/start": # Створення реакції бота на повідомлення "/start"
+        keyboard = telebot.types.InlineKeyboardMarkup() # Створення шаблону клавіатури вибору мови 
+        key_ru = telebot.types.InlineKeyboardButton(text = "Українська", callback_data = 'uk') # Формування кнопки "Українська"
+        keyboard.add(key_ru) # Додавання кнопки "Українська" на шаблон клавіатури
+        key_en = telebot.types.InlineKeyboardButton(text = "English", callback_data = 'en') # Формування кнопки "English"
+        keyboard.add(key_en) # Додавання кнопки "English" на шаблон клавіатури
+	# Повідомлення, що відправляється при старті бота
         bot.send_message(
 		message.from_user.id,
 		"Бот здатен перетворювати текст на аудіо. Вибери мову, після чого введи повідомлення для отримання результату",
 		reply_markup = keyboard)
 
     else:
-        tmp = gTTS(text = message.text, lang = language)
-        tmp.save('tmp.ogg')
+        tmp = gTTS(text = message.text, lang = language) # використання бібліотеки для перетворення тексту з повідомлення на аудіо
+        tmp.save('tmp.ogg') # Створення тимчасового файлу для запису отриманого аудіо
         audio = open('tmp.ogg', 'rb')
-        bot.send_audio(message.from_user.id, audio)
+        bot.send_audio(message.from_user.id, audio) # Відправка готового аудіо у вигляді голосового повідомлення
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     global language
-    if call.data == "en":
+    if call.data == "en": # Якщо виклик з кнопки буде "en", то мова зміниться на англійську
         language = call.data
+	# Відправка повідомлення про вдалу зміну мови
         bot.send_message(call.message.chat.id, "Мова обробки успішно змінена на англійську")
-    elif call.data == "uk":
+    elif call.data == "uk": # Якщо виклик з кнопки буде "uk", то мова зміниться на українську
         language = call.data
+	# Відправка повідомлення про вдалу зміну мови
         bot.send_message(call.message.chat.id, "Мова обробки успішно змінена на українську")
         
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0) # Для безпереривного функціонування бота
